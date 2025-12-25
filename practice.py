c4be5683724180a4c38a4e2eadb1d0d3d34c48fa
Ali <aliiqbalansari2@gmail.com>
2025-12-25 09:55:18 +0500

    Implement multiplication table using while and for loops
    Added both while and for loop implementations to print multiplication tables.

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
+#Using While Loop Created by Table in python:
 n = int(input("Enter your number: "))
 i = 1
 
@@ -5,10 +6,11 @@ while i < 11:
     print(n, "x", i, "=", n * i)
     i += 1
 
-
+#Using for Loop Created by Table in python:
 n = int(input("Enter your number: "))
 i=1
 
 for i in range(1,11):
     print(n, "x", i, "=", n * i)
     
+
